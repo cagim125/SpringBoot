ae16c4c3c3ac231a31d41fa8ba13e434d25dc6bd
cagim125 <cagim30@gmail.com>
2024-09-13 01:53:34 +0900

    상세페이지, 수정페이지 API 추가

--- src/main/java/com/baeksoo/shop/item/ItemService.java
+++ src/main/java/com/baeksoo/shop/item/ItemService.java
@@ -6,6 +6,7 @@ import org.hibernate.cache.spi.support.AbstractReadWriteAccess;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,18 +14,33 @@ public class ItemService {
 
     private final ItemRepository itemRepository;
 
+    // 글조회
     public List<ItemEntity> itemFindAll() {
         List<ItemEntity> result =  itemRepository.findAll();
         return result;
     }
 
+    // 글작성
     public ItemEntity createItem(ItemDto.Request request) {
         ItemEntity item = ItemEntity.builder()
                 .title(request.getTitle())
                 .price(request.getPrice())
                 .build();
         return itemRepository.save(item);
+    }
 
+    // 상세페이지
+    public Optional<ItemEntity> detailItem(Long id) {
+        return itemRepository.findById(id);
+    }
+
+    // 글수정
+    public void editItem(Long id, String title, Integer price) {
+        ItemEntity item = new ItemEntity();
+        item.setId(id);
+        item.setTitle(title);
+        item.setPrice(price);
+        this.itemRepository.save(item);
     }
 
 }
