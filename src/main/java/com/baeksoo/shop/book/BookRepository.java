0132d8af24e5573f7a6c96ee40d1488aea466647
cagim125 <cagim30@gmail.com>
2024-09-19 00:32:38 +0900

    Book LIMIT 100 네이티브 쿼리 추가

--- src/main/java/com/baeksoo/shop/book/BookRepository.java
+++ src/main/java/com/baeksoo/shop/book/BookRepository.java
@@ -1,8 +1,14 @@
 package com.baeksoo.shop.book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    @Query(value = "SELECT * FROM book ORDER BY title LIMIT 100", nativeQuery = true)
+    List<Book> findLimit100Book();
 }
