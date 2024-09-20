ffa56c49dcb822171924bc465658128419c9f851
cagim125 <cagim30@gmail.com>
2024-09-20 22:28:52 +0900

    멤버 변수 변경

--- src/main/java/com/baeksoo/shop/member/Member.java
+++ src/main/java/com/baeksoo/shop/member/Member.java
@@ -1,25 +1,26 @@
 package com.baeksoo.shop.member;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Data;
-import lombok.ToString;
+import jakarta.persistence.*;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
-@Entity
+@Builder
 @Data
 @ToString
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
-    private String email;
+    @Column(unique = true)
+    private String username;
+    private String password;
+    private String displayName;
 
     @CreationTimestamp
     private LocalDateTime created;
