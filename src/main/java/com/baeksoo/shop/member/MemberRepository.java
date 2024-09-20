dd4a106a485807228617be830f10ca14a032d665
cagim125 <cagim30@gmail.com>
2024-09-20 22:29:09 +0900

    existsByUsername, findByUsername 추가

--- src/main/java/com/baeksoo/shop/member/MemberRepository.java
+++ src/main/java/com/baeksoo/shop/member/MemberRepository.java
@@ -1,8 +1,13 @@
 package com.baeksoo.shop.member;
 
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    boolean existsByUsername(String username);
+    Optional<Member> findByUsername(String username);
 }
