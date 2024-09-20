58be3f0479cf344a0626e2d92d3a88ad6ef668b1
cagim125 <cagim30@gmail.com>
2024-09-20 22:30:42 +0900

    회원가입 추가

---
+++ src/main/java/com/baeksoo/shop/member/MemberService.java
@@ -0,0 +1,28 @@
+package com.baeksoo.shop.member;
+
+import com.baeksoo.shop.exception.DuplicateMemberException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public void signUp(MemberReqDto request) {
+        if (memberRepository.existsByUsername(request.getUsername())) {
+            throw new DuplicateMemberException("이미 존재하는 회원입니다.");
+        }
+
+        var hash = passwordEncoder.encode(request.getPassword());
+        Member member = Member.builder()
+                .username(request.getUsername())
+                .password(hash)
+                .displayName(request.getDisplayName())
+                .build();
+
+        memberRepository.save(member);
+    }
+}
