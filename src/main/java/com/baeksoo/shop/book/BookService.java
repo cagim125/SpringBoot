1dff7c9b51c4df9f3d1f7102e1696695ca2b240c
cagim125 <cagim30@gmail.com>
2024-09-24 18:41:46 +0900

    BookService Layer 추가

---
+++ src/main/java/com/baeksoo/shop/book/BookService.java
@@ -0,0 +1,17 @@
+package com.baeksoo.shop.book;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BookService {
+    private final BookRepository bookRepository;
+
+    public List<Book> findLimit30() {
+        return bookRepository.findLimit30Book();
+    }
+
+}
